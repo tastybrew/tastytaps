107c8c577f29c85694d51e9a2761d8e71af26e46
Rob Hudson <rob@tastybrew.com>
2015-04-06 21:57:37 -0700

    Drop the admin

--- tastytaps/settings.py
+++ tastytaps/settings.py
@@ -16,7 +16,6 @@ TEMPLATE_DEBUG = DEBUG
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
