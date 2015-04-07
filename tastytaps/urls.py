2b477a2f4cbbcb1ac25c48ace0597d3d5713cafd
Rob Hudson <rob@tastybrew.com>
2015-04-06 22:02:40 -0700

    Drop the admin for srs this time

--- tastytaps/urls.py
+++ tastytaps/urls.py
@@ -1,10 +1,8 @@
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import url
 
 from .views import HomePageView
 
 
 urlpatterns = [
     url(r'^$', HomePageView.as_view(), name='home'),
-    url(r'^admin/', include(admin.site.urls)),
 ]
